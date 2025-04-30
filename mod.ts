a953b3be60621d05f962e09f034dd142da5850b1
Gnlow <gnlow@pm.me>
2025-05-01 01:52:12 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -4,15 +4,57 @@ using file = await Deno.open("./temp/k.kml")
 
 const document = parse(file)
 
-interface Info {
-    path: string
+type Info = Path | Point
+
+interface Path {
+    continent: string
+    country: string
+    state: string
+    
+    owner: string
+    line: string
+    name: string
+}
+
+interface Point {
+    category: string
+    state: string
+    
+    owner: string
+    line: string
+    name: string
 }
 
 const makeInfo =
-(path: string[]) =>
-({ name }: Placemark): Info => ({
-    path: path.slice(1).join("/")+"/"+name,
-})
+([_, type, ...path]: string[]) =>
+({ name }: Placemark): Path | Point => {
+    if (type == "Path") {
+        const [continent, country, state] = path
+        const [owner, line] = path.slice(-2)
+        return {
+            continent,
+            country,
+            state,
+            owner,
+            line,
+            name,
+        } as Path
+    }
+
+    if (type == "Point") {
+        const [category, state] = path
+        const [owner, line] = path.slice(-2)
+        return {
+            category,
+            state,
+            owner,
+            line,
+            name,
+        } as Point
+    }
+    console.log(type)
+    throw 0
+}
 
 const walk =
 (path: string[]) =>
@@ -26,5 +68,15 @@ const walk =
 ]
 
 const result = walk([])(document)
+    .filter(x => 1
+        //&& x.data[0] == "Point"
+    )
 
-console.log(result)
+import { stringify } from "https://esm.sh/jsr/@std/csv@1.0.6"
+
+await Deno.writeTextFile("./temp/k.tsv",
+    stringify(result as any, {
+        columns: ["line", "name"],
+        separator: "\t",
+    })
+)
